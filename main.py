9fad2123301ca98cab9d02941087933d2b80d6ca
cominger <cominger@hotmail.com>
2019-01-16 00:48:48 +0900

    main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse, os, pickle
 from nets.GAN import GAN
 from nets.WGAN import WGAN
+from nets.DiracGAN import DiracGAN
+from nets.CNN import CNN
 
 import pdb
 
@@ -17,9 +19,9 @@ def parse_args():
 	desc = "Pytorch implementation of GAN collections"
 	parser = argparse.ArgumentParser(description=desc)
 
-	parser.add_argument('--gan_type', type=str, default='GAN', choices=['GAN', 'WGAN'], help='The type of GAN')#, required=True)
-	parser.add_argument('--dataset', type=str, default='ImageNet', choices=['mnist','cifar10'], help='The name of dataset')
-	parser.add_argument('--dataroot_dir', type=str, default='data', help='root path of data')
+	parser.add_argument('--model_type', type=str, default='GAN', choices=['GAN', 'WGAN','DiracGAN','CNN','RNN'], help='The type of GAN')#, required=True)
+	parser.add_argument('--dataset', type=str, default='cifar10', choices=['mnist','cifar10'], help='The name of dataset')
+	parser.add_argument('--dataroot_dir', type=str, default='./data', help='root path of data')
 	parser.add_argument('--epoch', type=int, default=25, help='The number of epochs to run')
 	parser.add_argument('--batch_size', type=int, default=64, help='The size of batch')
 	parser.add_argument('--sample_num', type=int, default=64, help='The number of samples to test')
@@ -48,17 +50,13 @@ def check_args(opts):
 	if not os.path.exists(opts.result_dir):
 		os.makedirs(opts.result_dir)
 
-	# --result_dir
-	if not os.path.exists(opts.log_dir):
-		os.makedirs(opts.log_dir)
-
 	# --comment
 	if len(opts.comment)>0:
 		print( "comment: " + opts.comment )
 		comment_part = '_'+opts.comment
 	else:
 		comment_part = ''
-	tempconcat = opts.gan_type+option_part+comment_part
+	tempconcat = opts.model_type+comment_part
 	print( 'models and loss plot -> ' + os.path.join( opts.save_dir, opts.dataset, tempconcat ) )
 	print( 'results -> ' + os.path.join( opts.result_dir, opts.dataset, tempconcat ) )
 
@@ -85,20 +83,26 @@ def main():
 	if opts is None:
 		print("There is no opts!!")		
 		exit()
-	
-	if opts.gan_type == 'GAN':
-		gan = GAN(opts)
-	elif opts.gan_type == 'WGAN':
-		gan = WGAN(opts)
+
+	if opts.model_type == 'CNN':
+		model = CNN(opts)
+	elif opts.model_type == 'RNN':
+		model = RNN(opts)	
+	elif opts.model_type == 'GAN':
+		model = GAN(opts)
+	elif opts.model_type == 'WGAN':
+		model = WGAN(opts)
+	elif opts.model_type == 'DiracGAN':
+		model = WGAN(opts)
 	else:
-		raise Exception("[!] There is no option for " + opts.gan_type)
+		raise Exception("[!] There is no option for " + opts.model_type)
 
 	if opts.type == 'train':
-		gan.train()
+		model.train()
 		print("[*] Training finished")
 		
 	elif opts.type == 'test':
-		gan.test()
+		model.test()
 		print("[*] Test finished")
 
 if __name__ == '__main__':
